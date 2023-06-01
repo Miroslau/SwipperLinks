c44da0aca83cdc283eba39524949f7de4769c2c4
Miroslau <mira2408@mail.ru>
2023-06-01 10:35:47 +0200

    feat: the layout has been created

--- src/screens/main-screen/main-screen.tsx
+++ src/screens/main-screen/main-screen.tsx
@@ -1,10 +1,20 @@
 import React, {FC} from 'react';
 import {View, Text} from 'react-native';
+import {MainScreenStyled} from './main-screen.styled';
+import Title from '../../components/title/title';
+
+const TITLE: string = 'Swipper Links';
 
 const MainScreen: FC = () => {
   return (
-    <View>
-      <Text>Main Screen</Text>
+    <View style={MainScreenStyled.container}>
+      <Title title={TITLE} />
+      <View>
+        <Text>Links</Text>
+      </View>
+      <View>
+        <Text>buttons</Text>
+      </View>
     </View>
   );
 };
